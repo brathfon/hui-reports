908710acd2b363097c76f8e2fd84911913899d8e
Bill Rathfon <billrathfon@gmail.com>
2021-06-22 13:39:13 -0400

    Finally finished up 1st quarter 2021 WQX-EPA load. Had to wait to get a login.

--- scripts/wqx/run.sh
+++ scripts/wqx/run.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 
-resultsDir=20210205b-wqp-4th-quarter-2020-post-load
-basename=20210205-add-4th-quarter-2020-temp
+wqxFileDir=20210608a-wqx-1st-quarter-2021-sync-prep
+basename=20210608-add-1st-quarter-2021-0
 
 ./create-wqx-activities-and-results.js  \
     -o ./load-files  \
     -b $basename  \
     -g ~/development/water-quality/hui-reports/data/google-drive-downloads/ \
     -n ~/development/water-quality/hui-reports/data/nutrient-data/  \
-    -w ~/development/water-quality/water-quality-data/storet/$resultsDir/ResultsExport.tsv 
+    -w ~/development/water-quality/water-quality-data/storet/$wqxFileDir/ResultsExport.tsv 
