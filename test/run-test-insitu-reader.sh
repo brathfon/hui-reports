3d5959319a320814062b664dcaca32fb4e6f11e4
Bill Rathfon <billrathfon@gmail.com>
2023-08-05 14:07:31 -0400

    check in before merge of changes for Aqualink script addition

--- test/run-test-insitu-reader.sh
+++ test/run-test-insitu-reader.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 # first session from each team
-input=./one-session-baseline-data/google-drive-downloads
-basename=one-session-each-sheets-test-with-parser_3
+#input=./one-session-baseline-data/google-drive-downloads
+#basename=one-session-each-sheets-test-with-parser_4
 
 # old "full" data (just sessions 78w to 53s)
 #input=./baseline-data/google-drive-downloads
 
 # new full baseline, from same spreadsheet being used in GAS
-#input=./sessions-93w-68s-baseline-data
-#basename=93w-68s-sheets-test-with-parser-2
+input=./sessions-93w-68s-baseline-data/google-drive-downloads
+basename=93w-68s-sheets-test-with-parser-3
 
 
 outputDir=./results
